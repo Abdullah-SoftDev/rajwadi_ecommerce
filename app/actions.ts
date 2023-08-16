1faa632b8fe285bcee5406a35141ea604a4c5b0c
Abdullah-SoftDev <siddabdullahofficial@gmail.com>
2023-08-16 11:55:18 +0530

    Try image uploading using server actions getting firebase error

--- app/actions.ts
+++ app/actions.ts
@@ -1,4 +1,5 @@
 'use server'
+
 export async function checkServiceability(pincode: string) {
     try {
         const response = await fetch('http://localhost:3000/api/pincodes');
@@ -9,3 +10,23 @@ export async function checkServiceability(pincode: string) {
         return false;
     }
 }
+
+// export const handleSubmitImage = async (data: Product) => {
+//     const storageRef = ref(storage, `images/${Timestamp.now().seconds}/`);
+//     const downloadURLs: string[] = [];
+//     for (const item of data?.productImages) {
+//         if (!(typeof item === 'string')) {
+//             const file = item as File;
+//             const filePath = `${storageRef.fullPath}/${file.name}`;
+
+//             await uploadBytes(ref(storage, filePath), item);
+
+//             const fileRef = ref(storage, filePath);
+//             const downloadURL = await getDownloadURL(fileRef);
+
+//             downloadURLs.push(downloadURL);
+//         }
+//     }
+//     console.log(downloadURLs)
+//     return downloadURLs;
+// };
