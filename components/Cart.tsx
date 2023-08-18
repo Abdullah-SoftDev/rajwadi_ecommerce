2255438f4709d16bc1c072e5e27b5aa011a5032a
Abdullah-SoftDev <siddabdullahofficial@gmail.com>
2023-08-18 18:07:32 +0530

    Added subtotal part in cart

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -1,8 +1,7 @@
 import { Fragment, useEffect, useState } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
-import { CartProps } from "@/types/typescript.types";
-import Link from "next/link";
+import { CartProps, Product } from "@/types/typescript.types";
 import { auth, db } from "@/firebase/firebaseConfig";
 import { query, collection, orderBy, deleteDoc, doc } from "firebase/firestore";
 import { useAuthState } from "react-firebase-hooks/auth";
@@ -16,38 +15,37 @@ const Cart = ({ cartOpen, setCartOpen }: CartProps) => {
   const [userData, setUserData] = useState<User | null>(null);
 
   useEffect(() => {
-      if (user) {
-          fetchUserData(user, setUserData);
-      }
+    if (user) {
+      fetchUserData(user, setUserData);
+    }
   }, [user]);
-  
-  const cartQuery = query(
-    collection(db, `users/${user?.uid}/cart`),
-    orderBy('createdAt')
-  );
 
+  const cartQuery = query(collection(db, `users/${user?.uid}/cart`),orderBy("createdAt"));
   const [cartData, loading] = useCollectionData(cartQuery);
 
   const handelRemoveFromCart = async (id: string) => {
     if (!user) {
-      alert('Login first');
+      alert("Login first");
       return;
     }
     await deleteDoc(doc(db, `users/${user?.uid}/cart/${id}`));
-  }
+  };
 
   const handelIncrementQty = async (id: string) => {
     if (userData) {
       await incrementQty(userData, id);
     }
-  }
+  };
 
   const handelDecrementQty = async (id: string) => {
     if (userData) {
       await decrementQty(userData, id);
     }
-  }
+  };
 
+  const totalSum = cartData?.reduce((accumulator, item) => {
+    return accumulator + item.price * item.quantity;
+  }, 0);
 
   return (
     <Transition.Root show={cartOpen} as={Fragment}>
@@ -96,11 +94,11 @@ const Cart = ({ cartOpen, setCartOpen }: CartProps) => {
                         </div>
                       </div>
 
-                      {(cartData?.length === 0) ?
+                      {cartData?.length === 0 ? (
                         <h2 className="text-xl text-red-500 m-auto py-4">
                           No products available at the moment.
                         </h2>
-                        :
+                      ) : (
                         <div className="mt-8">
                           <div className="flow-root">
                             <ul
@@ -125,35 +123,47 @@ const Cart = ({ cartOpen, setCartOpen }: CartProps) => {
                                         </h3>
                                         <p className="ml-4">₹{product.price}</p>
                                       </div>
-                                      <p className="mt-1 text-sm text-gray-500">Size: {product.selectedSize}</p>
+                                      <p className="mt-1 text-sm text-gray-500">
+                                        Size: {product.selectedSize}
+                                      </p>
                                     </div>
                                     <div className="flex flex-1 items-end justify-between text-sm">
                                       <div className="flex items-center">
-                                      <form action={() => handelDecrementQty(product.id)}>
-                                        <button
-                                          type="submit"
-                                          className="p-1 border border-gray-300 rounded-full hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                                        <form
+                                          action={() =>
+                                            handelDecrementQty(product.id)
+                                          }
                                         >
-                                          <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            fill="none"
-                                            viewBox="0 0 24 24"
-                                            stroke="currentColor"
-                                            className="h-4 w-4"
+                                          <button
+                                            type="submit"
+                                            className="p-1 border border-gray-300 rounded-full hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-indigo-500"
                                           >
-                                            <path
-                                              strokeLinecap="round"
-                                              strokeLinejoin="round"
-                                              strokeWidth={2}
-                                              d="M20 12H4"
-                                            />
-                                          </svg>
-                                        </button>
+                                            <svg
+                                              xmlns="http://www.w3.org/2000/svg"
+                                              fill="none"
+                                              viewBox="0 0 24 24"
+                                              stroke="currentColor"
+                                              className="h-4 w-4"
+                                            >
+                                              <path
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                strokeWidth={2}
+                                                d="M20 12H4"
+                                              />
+                                            </svg>
+                                          </button>
                                         </form>
 
-                                        <p className="mx-2 text-black">{product?.quantity}</p>
+                                        <p className="mx-2 text-black">
+                                          {product?.quantity}
+                                        </p>
 
-                                        <form action={() => handelIncrementQty(product.id)}>
+                                        <form
+                                          action={() =>
+                                            handelIncrementQty(product.id)
+                                          }
+                                        >
                                           <button
                                             type="submit"
                                             className="p-1 border border-gray-300 rounded-full hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-indigo-500"
@@ -178,7 +188,9 @@ const Cart = ({ cartOpen, setCartOpen }: CartProps) => {
 
                                       <div className="flex">
                                         <button
-                                          onClick={() => handelRemoveFromCart(product.id)}
+                                          onClick={() =>
+                                            handelRemoveFromCart(product.id)
+                                          }
                                           type="button"
                                           className="font-medium text-indigo-600 hover:text-indigo-500"
                                         >
@@ -192,40 +204,38 @@ const Cart = ({ cartOpen, setCartOpen }: CartProps) => {
                             </ul>
                           </div>
                         </div>
-                      }
+                      )}
                     </div>
 
-                    <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
-                      <div className="flex justify-between text-base font-medium text-gray-900">
-                        <p>Subtotal</p>
-                        <p>$262.00</p>
-                      </div>
-                      <p className="mt-0.5 text-sm text-gray-500">
-                        Shipping and taxes calculated at checkout.
-                      </p>
-                      <div className="mt-6">
-                        <Link
-                          href="/"
-                          className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
-                        >
-                          Checkout
-                        </Link>
-                      </div>
-                      <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
-                        <p>
-                          or
-                          <button
-                            type="button"
-                            className="font-medium text-indigo-600 hover:text-indigo-500"
-                            onClick={() => setCartOpen(false)}
-                          >
-                            Continue Shopping
-                            <span aria-hidden="true"> &rarr;</span>
-                          </button>
+                    {!(cartData?.length === 0) && (
+                      <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
+                        <div className="flex justify-between text-base font-medium text-gray-900">
+                          <p>Subtotal</p>
+                          <p>₹{totalSum}</p>
+                        </div>
+                        <p className="mt-0.5 text-sm text-gray-500">
+                          Shipping and taxes calculated at checkout.
                         </p>
+                        <div className="mt-6">
+                          <button className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700">
+                            Checkout
+                          </button>
+                        </div>
+                        <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
+                          <p>
+                            or
+                            <button
+                              type="button"
+                              className="font-medium text-indigo-600 hover:text-indigo-500"
+                              onClick={() => setCartOpen(true)}
+                            >
+                              Continue Shopping
+                              <span aria-hidden="true"> &rarr;</span>
+                            </button>
+                          </p>
+                        </div>
                       </div>
-                    </div>
-
+                    )}
                   </div>
                 </Dialog.Panel>
               </Transition.Child>
