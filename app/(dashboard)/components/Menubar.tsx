fcb8bb212c8b3340b22437e291db61fc5725c7ca
Abdullah-SoftDev <siddabdullahofficial@gmail.com>
2023-08-27 19:02:31 +0530

    Modify Menubar in adminpannel

--- app/(dashboard)/components/Menubar.tsx
+++ app/(dashboard)/components/Menubar.tsx
@@ -1,24 +1,26 @@
 import Link from "next/link";
-import React from "react";
+
 const names = [
-    { name: "Add product", path: "/addProduct" },
-    { name: "Recent orders", path: "/recentOrders" },
-    { name: "Veiw all products", path: "/viewAllProducts" },
-    { name: "Banner images", path: "/addBannerImages" },
-    { name: "Our customers", path: "/ourCustomers" },
+    { name: "Add product", path: "/addProduct", color: "bg-blue-100" },
+    { name: "Recent orders", path: "/recentOrders", color: "bg-green-100" },
+    { name: "View all products", path: "/viewAllProducts", color: "bg-yellow-100" },
+    { name: "Banner images", path: "/addBannerImages", color: "bg-red-100" },
+    { name: "Our customers", path: "/ourCustomers", color: "bg-purple-100" },
 ];
 
 const Menubar = () => {
     return (
-        <>
-            <nav className="mx-auto max-w-6xl flex p-4 text-xl overflow-x-auto whitespace-nowrap space-x-6">
+        <div className="overflow-x-auto">
+            <nav className="mx-auto max-w-6xl flex p-4 text-xl whitespace-nowrap space-x-6 overflow-x-auto">
                 {names.map((item, index) => (
-                    <Link href={item.path} key={index} className="cursor-pointer hover:text-purple-500">
-                        {item.name}
+                    <Link href={item.path} key={index} className="cursor-pointer">
+                        <span className={`inline-flex items-center px-2.5 py-0.5 rounded-full ${item.color} text-purple-700 hover:text-gray-700`}>
+                            {item.name}
+                        </span>
                     </Link>
                 ))}
             </nav>
-        </>
+        </div>
     );
 };
 
