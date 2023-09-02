c67f8967570d62ae52cbf0f6d1bbf35c03e9e55d
Abdullah-SoftDev <siddabdullahofficial@gmail.com>
2023-09-02 17:11:26 +0530

    Fix the loading state in slug page

---
+++ app/[category]/[slug]/loading.tsx
@@ -0,0 +1,9 @@
+const loading = () => {
+  return (
+    <div className="min-h-screen">
+        <p className="text-2xl text-purple-500 items-center text-center justify-center animate-pulse">Loading..</p>
+    </div>
+  )
+}
+
+export default loading
